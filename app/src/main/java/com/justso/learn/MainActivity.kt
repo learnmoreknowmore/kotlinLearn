fa29c037612bd68584fabdc0052371e8d9365c54
learnmoreknowmore <Cs416722>
2020-07-03 18:36:10 +0800

    init

--- app/src/main/java/com/justso/learn/MainActivity.kt
+++ app/src/main/java/com/justso/learn/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
 import com.justso.learn.databinding.ActivityMainBinding
 
+/**
+ * https://github.com/android/architecture-components-samples
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
